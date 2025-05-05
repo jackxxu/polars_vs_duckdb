f246a17e7493ec80aed7b55676a3ef6d6c445c1d
Jack Xu <jackxxu@gmail.com>
2025-05-05 18:54:59 -0400

    add pandas

--- perf_test.py
+++ perf_test.py
@@ -1,6 +1,7 @@
 import glob
 import duckdb
 import polars as pl
+import pandas as pd
 from timer import timer
 from data_gen import generate_parquets
 
@@ -39,10 +40,27 @@ def merge_duckdb(files):
   con = duckdb.connect()
   return con.execute(query).fetchdf()
 
+
+@timer
+def merge_pandas(files):
+    # Read first file
+    df = pd.read_parquet(files[0])[['id', 'value']].rename(columns={'value': 'value_0'})
+
+    # Iteratively join the rest on 'id' and rename their 'value' columns
+    for i, f in enumerate(files[1:], start=1):
+        df_new = pd.read_parquet(f)[['id', 'value']].rename(columns={'value': f'value_{i}'})
+        df = pd.merge(df, df_new, on='id', how='inner')
+
+    return df
+
+pandas_df = merge_pandas(files)
 polars_df = merge_polars(files)
 duckdb_df = merge_duckdb(files)
 
-# # compare the two dataframes using datacompy
-# import datacompy
-# compare = datacompy.Compare(polars_df, duckdb_df, on_index=True)
-# print(compare.report())
+# compare the two dataframes using datacompy
+import datacompy
+compare = datacompy.Compare(polars_df, duckdb_df, on_index=True)
+assert compare.matches(), "DataFrames are not equal"
+
+compare = datacompy.Compare(polars_df, pandas_df, on_index=True)
+assert compare.matches(), "DataFrames are not equal"
